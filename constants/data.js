5249a7778b0052ceaa1d0f7484f47ee0d0f9c061
Lachie King <kingy00112233@gmail.com>
2026-06-10 15:48:45 +1000

    add Favourites page. add data.js to use mock data

---
+++ constants/data.js
@@ -0,0 +1,26 @@
+export const MOCK_FAVOURITES = [
+    {
+        business_id: "12",
+        name: "Lucky Strike Bowling",
+        status: "Active",
+        rating: "4.7",
+        distance: "0.5km",
+        time: "Open until 1am",
+    },
+    {
+        business_id: "13",
+        name: "Coffee Central",
+        status: "Active",
+        rating: "4.5",
+        distance: "1.2km",
+        time: "Open until 5pm",
+    },
+    {
+        business_id: 14,
+        name: "Iron Mikes Premium Karaoke",
+        status: "Active",
+        rating: "4.9",
+        distance: "0.7km",
+        time: "Open Until 1am",
+    }
+];
