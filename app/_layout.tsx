2c564ad5a0735677cb76db439cad0c8f434ddf7d
Lachie King <kingy00112233@gmail.com>
2026-06-11 13:58:42 +1000

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,8 +6,6 @@ import {
   PaperProvider
 } from 'react-native-paper';
 import 'react-native-reanimated';
-
-
 const theme = {
   ...PaperDefaultTheme,
   colors: {
@@ -15,7 +13,7 @@ const theme = {
     background: '#142140',
     primary: '#FF6B6B',
     tertiary: '#8E8E93',
-    onPrimary: '#2a3652'
+    onPrimary: '#2a3652',
   }
 }
 
@@ -31,7 +29,8 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" options={{ headerShown: true }} />
         <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="forgot-password" options={{ headerShown: false }} />
-        <Stack.Screen name="sign-up" options={{ headerShown: false, animation: 'none' }} />
+        <Stack.Screen name="sign-up-customer" options={{ headerShown: false, animation: 'none' }} />
+        <Stack.Screen name="sign-up-business" options={{ headerShown: false, animation: 'none' }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
         <Stack.Screen name="profile" options={{ headerShown: true}} />
         <Stack.Screen name="contact-us" options={{ headerShown: true }} />
