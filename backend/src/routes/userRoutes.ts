c6f4c664c53fbb94330bef0b02070e8dc9acc9c3
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-06-11 16:29:32 +1000

    add PUT /users/profile endpoint & remove is_owner from User

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/authenticate.js";
-import { getProfile } from "../controllers/userController.js";
+import { getProfile, updateProfile } from "../controllers/userController/userController.js";
 
 const router = Router();
 
 router.get("/profile", authenticate, getProfile);
+router.put("/profile", authenticate, updateProfile);
 
 export default router;
